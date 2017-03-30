801cbb7a4534511eff97e01e5a399abdf26121d4
Eri Irawan <eryirawan91@gmail.com>
2017-03-30 16:06:43 +0700

    mat state to props & dispatch to prop

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,21 +1,51 @@
 import React from 'react'
-import logo from '../logo.svg';
+import { connect } from 'react-redux'
 
-const PeopleList = (props) => {
-  return (
-    <div className="peoples">
-      {props.peoples.length === 0 && <img src={logo} className="App-logo" alt="logo" />}
-      <ul>
-        {
-          props.peoples.map((item, index) => {
-            return (
-              <p key={index}>{item.name}</p>
-            )
-          })
-        }
-      </ul>
-    </div>
-  )
+import logo from '../logo.svg';
+import { dispatchPeoples } from '../actions'
+
+class PeopleList extends React.Component {
+  componentDidMount(){
+    fetch('https://swapi.co/api/people/')
+      .then((response, err) => {
+        if (err) throw err
+        return response.json()
+      })
+      .then(data => {
+        this.props.loadPeople(data.results)
+      })
+  }
+
+  render (){
+    return (
+      <div className="peoples">
+        {this.props.people.length > 0 ?
+        <ul>
+          {
+            this.props.people.map((item, index) => {
+              return (
+                <p key={index}>{item.name}</p>
+              )
+            })
+          }
+        </ul>
+        : <img src={logo} className="App-logo" alt="logo" />}
+      </div>
+    )
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    people : state.people
+  }
 }
 
-export default PeopleList
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadPeople : (data) => {
+      dispatch(dispatchPeoples(data))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PeopleList)
