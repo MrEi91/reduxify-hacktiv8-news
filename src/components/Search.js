7d688e73d8702525119d4535ff03745ca26c74f4
Eri Irawan <eryirawan91@gmail.com>
2017-03-30 16:07:55 +0700

    add map dispatch to props, keyword

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import { dispatchSearch } from '../actions'
 
 const Search = (props) => {
   return(
     <form>
-      <input type="text" onChange={props.handleChange} />
+      <input type="text" onChange={(event) => props.keyWord(event.target.value)} />
     </form>
   )
 }
 
-export default Search
+const mapDispatchToProps = (dispatch) => {
+  return {
+    keyWord : (data) => {
+      dispatch(dispatchSearch(data))
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Search)
